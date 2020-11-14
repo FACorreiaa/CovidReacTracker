0bbb25a231600966ff7df58934f2cb8ee62ffd19
FAcorreiaa <fernandocorreia316@gmail.com>
2020-11-14 07:47:40 +0000

    added font types

--- src/Pages/LandingPage_/Footer.tsx
+++ src/Pages/LandingPage_/Footer.tsx
@@ -1,9 +1,10 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import "fontsource-roboto";
+
 //h-footer
 export default function Footer() {
   return (
-    <footer className="bg-footer-main justify-center ">
+    <footer className="bg-footer-main justify-center font-roboto">
       <section className="w-footer flex justify-between m-footer text-footer-color">
         <section>
           <h1 className="text-footer-title font-footer  text-footer-color">
